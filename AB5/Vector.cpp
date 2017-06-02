3d100615824a6ac1d7fe3d967fc8ac1313c17e06
metraf <firsov.tyoma@gmail.com>
2017-06-02 12:13:12 +0200

    vector start

---
+++ AB5/Vector.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+class Vector2D{
+	int x;
+	int y;
+	public:
+		Vector2D(){};
+		Vector2D(int x, int y){
+			this->x = x;
+			this->y = y;
+		}
+		void setX(int x){
+			this->x = x;
+		}
+		void setY(int y){
+			this->y = y;
+		}
+
+		int getX(){
+			return x;
+		}
+		int getY(){
+			return y;
+		}
+	//	static double length(){
+	//		return sqrt(x*x + y*y);
+	//	}
+
+};
+
+int main(){
+	Vector2D * vector = new Vector2D(5, 6);
+	cout << vector->getX();
+}
