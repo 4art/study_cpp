9af0bad980e53e63881c8307350a350c17e44145
metraf <firsov.tyoma@gmail.com>
2017-07-23 20:47:32 +0200

    added a1R.cpp with int vars

--- klausurVorbereitung/a1R.cpp
+++ klausurVorbereitung/a1R.cpp
@@ -7,23 +7,34 @@ class A{
 		int *i;
 	public:
 	 	A(int i){
-			this->i = &i;
-		}
-		/*A(A a){
 			this->i = new int;
-			this->i = a.getI();
-		}*/
+			*this->i = i;
+		}
+		A(A &a){
+			this->i = new int;
+			int b = a.getI();
+			this->i = &b;
+		}
+		
 		int getI(){
 			return *i;
 		}
 		static void print(A * a){
 			cout << a->getI() << endl;
 		}
+		A & operator= (A &a){
+			A temp(a);
+			cout << "equals" << endl;
+			return *this;
+		}
 };
 
 int main(){
 	A a(5);
-
+	A *b = new A(10);
+	A c(a);
 	A::print(&a);	
+	A::print(b);	
+	A::print(&c);	
 	return 0;
 }
